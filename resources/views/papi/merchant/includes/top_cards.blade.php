b0c783dda06ef299175019349b66f992a13ebb53
Felix mgeni <felixthobias@gmail.com>
2025-01-22 12:11:12 +0300

    Hello Felix

--- resources/views/papi/merchant/includes/top_cards.blade.php
+++ resources/views/papi/merchant/includes/top_cards.blade.php
@@ -16,11 +16,11 @@
             <div class="card-body pb-10">
                 <ul>
                     <li><i class="iconly-Document icli me-1"></i>
-                        <h5>{{count($transactions->where('type','credit')->where('status','success'))}}
+                        <h5>{{count($transactions->where('type','credit')->where('status','paid'))}}
                             Transactions</h5>
                     </li>
                     <li><i class="iconly-Wallet icli me-1"></i>
-                        <h5>{{number_format($transactions->where('type','credit')->where('status','success')->sum('amount'))}}
+                        <h5>{{number_format($transactions->where('type','credit')->where('status','paid')->sum('amount'))}}
                             TZS</h5>
                     </li>
                 </ul>
@@ -72,11 +72,11 @@
             <div class="card-body pb-10">
                 <ul>
                     <li><i class="iconly-Document icli me-1"></i>
-                        <h5>{{count($transactions->where('type','debit')->where('status','pending'))}}
+                        <h5>{{count($business->disbursements}}
                             Transactions</h5>
                     </li>
                     <li><i class="iconly-Wallet icli me-1"></i>
-                        <h5>{{number_format($transactions->where('type','debit')->where('status','pending')->sum('amount'))}}
+                        <h5>{{number_format($business->disbursements->sum('amount'))}}
                             TZS</h5>
                     </li>
                 </ul>
